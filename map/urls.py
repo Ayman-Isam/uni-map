bffa68b4304a218255767a685e5ed86f9824ad51
Ayman Isam <aymanisam26@gmail.com>
2023-12-08 12:28:01 +0200

    Final Commit

--- map/urls.py
+++ map/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add-marker/', views.add_marker, name='add_marker'),
+    path('add-marker/delete/<int:pk>', views.delete_marker, name='delete_marker'),
+    path('update-marker/', views.update_marker, name='update_marker'),
     path('api/markers', views.get_markers, name='get_markers'),
 ]
--- map/views.py
+++ map/views.py
@@ -1,26 +1,39 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import MarkerForm
 from django.http import JsonResponse
 from django.core import serializers
 from .models import Marker
+from django.views.decorators.csrf import csrf_exempt
 import json
 
 def home(request):
     return render(request, 'home.html')
 
 def add_marker(request):
+    markers = Marker.objects.all()
     if request.method == 'POST':
         form = MarkerForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
-        else:
-            return render(request, 'add_marker.html', {'form': form})
+            return redirect('add_marker')  
     else:
         form = MarkerForm()
-        return render(request, 'add_marker.html', {'form': form})
+    return render(request, 'add_marker.html', {'form': form, 'markers': markers})
     
 def get_markers(request):
     markers = Marker.objects.all()
     marker_list = serializers.serialize('json', markers)
     return JsonResponse(json.loads(marker_list), safe=False)
+
+def delete_marker(request, pk):
+    marker = get_object_or_404(Marker, pk=pk)
+    marker.delete()
+    return redirect('add_marker')
+
+@csrf_exempt
+def update_marker(request):
+    data = json.loads(request.body)
+    marker = Marker.objects.get(pk=data['id'])
+    setattr(marker, data['column'], data['value'])
+    marker.save()
+    return JsonResponse({'status': 'success'})
