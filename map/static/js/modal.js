915571ad8bbb0484fdfff19b77b2cbd407090d9a
Ayman Isam <aymanisam26@gmail.com>
2023-12-18 13:27:32 +0200

    Shift to ignore modal and small changes

--- map/static/js/modal.js
+++ map/static/js/modal.js
@@ -1,16 +1,15 @@
 const sections = document.querySelectorAll("section");
 
 sections.forEach(section => {
-    const overlay = section.querySelector(".overlay");
     const showBtn = section.querySelector(".show-modal");
     const closeBtn = section.querySelector(".close-btn");
 
-    showBtn.addEventListener("click", () => section.classList.add("active"));
-    closeBtn.addEventListener("click", () => section.classList.remove("active"));
-
-    overlay.addEventListener("click", (event) => {
-        if (event.target === overlay) {
-            section.classList.remove("active");
+    showBtn.addEventListener("click", (event) => {
+        if (event.shiftKey) {
+            window.location.href = showBtn.dataset.deleteUrl;
+        } else {
+            section.classList.add("active");
         }
     });
+    closeBtn.addEventListener("click", () => section.classList.remove("active"));
 });
